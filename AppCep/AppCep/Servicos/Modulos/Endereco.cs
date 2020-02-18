7416eec2d60135cfd1609d8c4e71cde4e2a91853
Rei Almeida <reiguitarra@gmail.com>
2020-02-18 19:58:45 -0300

    primeiro passo

---
+++ AppCep/AppCep/Servicos/Modulos/Endereco.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppCep.Servicos.Modulos
+{
+    public class Endereco
+    {
+
+        public string Cep { get; set; }
+        public string Logradouro { get; set; }
+        public string Complemento { get; set; }
+        public string Bairro { get; set; }
+        public string Localidade { get; set; }
+        public string UF { get; set; }
+        public string Unidade { get; set; }
+        public string IBGE { get; set; }
+        public string Gia { get; set; }
+
+        public Endereco()
+        {
+
+        }
+
+        public Endereco(string cep, string logradouro, string complemento, string bairro, string localidade, string uF, string unidade, string iBGE, string gia)
+        {
+            Cep = cep;
+            Logradouro = logradouro;
+            Complemento = complemento;
+            Bairro = bairro;
+            Localidade = localidade;
+            UF = uF;
+            Unidade = unidade;
+            IBGE = iBGE;
+            Gia = gia;
+        }
+    }
+}
---
+++ AppCep/AppCep/Servicos/ViaCep.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
+using AppCep.Servicos.Modulos;
+using Newtonsoft.Json;
+
+namespace AppCep.Servicos
+{
+    public class ViaCep
+    {
+
+        private static string EnderecoUrl = "https://viacep.com.br/ws/{0}/json/";
+
+
+        public static Endereco BuscarEndereco (string cep)
+        {
+
+            string novoEndereco = string.Format(EnderecoUrl, cep);
+
+            WebClient wc = new WebClient();
+
+            string conteudo = wc.DownloadString(novoEndereco);
+
+            Endereco end = JsonConvert.DeserializeObject<Endereco>(conteudo);
+
+            return end;
+
+        }
+
+    }
+}
